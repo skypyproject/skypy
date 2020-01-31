48496ac102574e8da22cc2815f504335d120ff7f
Ian Harrison <itrharrison@gmail.com>
2020-01-31 15:45:16 +0000

    barebones calculation of spectrum

--- skypy/linear/camb.py
+++ skypy/linear/camb.py
@@ -3,9 +3,9 @@ import camb as _camb
 
 
 def camb(wavenumber, redshift, cosmology):
-    """ Eisenstein-Hu fitting function for the linear matter
-    power spectrum with (or without) acoustic osscilations described in [1],
-    [2].
+    """ CAMB computation of the linear matter power spectrum, on a two
+    dimensional grid of wavenumber and redshift
+
     Parameters
     ----------
     wavenumber : array_like
@@ -23,16 +23,39 @@ def camb(wavenumber, redshift, cosmology):
     References
     ----------
 
+
     """
 
     print('Using CAMB %s installed at %s'%(_camb.__version__,os.path.dirname(_camb.__file__)))
 
     redshifts = np.atleast_1d(redshifts)
 
-    pars.WantTransfers = True
+    h2 = cosmology.h*cosmology.h
+
+    # ToDo: ensure astropy.cosmology can fully specify model
+    pars.set_cosmology(H0=cosmology.H0.value,
+                        ombh2=cosmology.Ob0*h2,
+                        omch2=cosmology.Odm0*h2,
+                        omk=cosmology.Ok0,
+                        TCMB=cosmology.Tcmb0.value,
+                        mnu=np.sum(cosmology.m_nu.value),
+                        standard_neutrino_neff=cosmology.Neff
+                        #tau=cosmology.tau
+                        )
+
+    pars.WantTransfer = True
+
+    # check redshifts decreasing (required by camb)
+    if redshifts[-1] > redshifts[0]:
+        redshifts = redshifts[::-1]
+
+    #pars.InitPower.ns = cosmology.n
+    
     pars.Transfer.PK_num_redshifts = len(list(redshifts))
     pars.Transfer.PK_redshifts = list(redshifts)
 
     results = _camb.get_results(pars)
-    kh, z, pk = results.get_matter_power_spectrum(minkh=wavenumber.min(), maxkh=wavenumber.max(), npoints=len(wavenumber))
+    
+    kh, z, power_spectrum = results.get_matter_power_spectrum(minkh=wavenumber.min(), maxkh=wavenumber.max(), npoints=len(wavenumber))
+
     return power_spectrum
