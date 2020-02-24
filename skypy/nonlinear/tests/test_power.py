1491ef78455c6a06995cf6783dea211483547cc7
Richard R <rrjbca@users.noreply.github.com>
2020-02-24 15:43:37 +0000

    Tests for halofit

--- skypy/nonlinear/tests/test_power.py
+++ skypy/nonlinear/tests/test_power.py
@@ -1,27 +1,99 @@
 import numpy as np
-from astropy.cosmology import FlatLambdaCDM
+from astropy.cosmology import Planck15
 from astropy.units import allclose
-from skypy.nonlinear.power import halofit
+import pytest
+from skypy.nonlinear.power import halofit_smith
+from skypy.nonlinear.power import halofit_takahashi
+from skypy.nonlinear.power import halofit_bird
 
 
 def test_halofit():
-    """Test a FlatLambdaCDM cosmology at redshift 0"""
-    # Inputs from camb linear
-    k = np.array([1.00000000e-04, 1.80870267e-03, 3.27140534e-02,
-                  5.91699957e-01, 1.01000000e+01])
-    z = 0.0
-    p = np.array([388.6725682632502, 6048.101178435452, 17332.199680774567,
-                  216.03287586930986, 0.21676249605280398])
-    cosmo = FlatLambdaCDM(H0=67.04, Om0=0.21479, Ob0=0.04895)
-    model = 'Takahashi'
+    """Test Smith, Takahashi and Bird Halofit models with Planck15 cosmology"""
 
-    # Test that output is a one-dimensional array
-    nl_power = halofit(k, z, p, cosmo, model)
-    assert nl_power.shape == p.shape
+    # Data for tests
+    k = np.logspace(-4.0, 0.0, 5)
+    z = np.linspace(0.0, 1.0, 2)
+    p = [[705.54997046, 262.14967329], [6474.60158058, 2405.66190924],
+         [37161.00990355, 13807.30920991], [9657.02613688, 3588.10339832],
+         [114.60445565, 42.58170486]]
+    ts = [[705.49028554, 262.13980433], [6469.19660893, 2404.7575993],
+          [36849.27940095, 13757.6857957], [9028.38832973, 3628.75093838],
+          [596.77542947, 110.06417004]]
+    tt = [[705.48895972, 262.14056282], [6469.02602087, 2404.83713061],
+          [36827.73510523, 13751.54825248], [9144.22074081, 3050.8774635],
+          [662.12415949, 60.6551114]]
+    tb = [[705.49030632, 262.13980233], [6469.19994215, 2404.75765353],
+          [36849.25053837, 13757.80854066], [9011.21623116, 3630.55684957],
+          [601.30939792, 111.5048721]]
+    linear_power = np.array(p)
+    truth_smith = np.array(ts)
+    truth_takahashi = np.array(tt)
+    truth_bird = np.array(tb)
 
-    # Test against precomputed values
-    precomputed_halo = np.array([3.88652482e+02, 6.04244677e+03,
-                                 1.71388132e+04, 3.83404914e+02,
-                                 8.64713011e+00])
+    # Non-linear power spectra from Smith, Takahashi and Bird models
+    nl_power_smith = halofit_smith(k, z, linear_power, Planck15)
+    nl_power_takahashi = halofit_takahashi(k, z, linear_power, Planck15)
+    nl_power_bird = halofit_bird(k, z, linear_power, Planck15)
 
-    assert allclose(nl_power, precomputed_halo)
+    # Test shape of outputs
+    assert np.shape(nl_power_smith) == np.shape(linear_power)
+    assert np.shape(nl_power_takahashi) == np.shape(linear_power)
+    assert np.shape(nl_power_bird) == np.shape(linear_power)
+
+    # Test outputs against precomputed values
+    assert allclose(nl_power_smith, truth_smith)
+    assert allclose(nl_power_takahashi, truth_takahashi)
+    assert allclose(nl_power_bird, truth_bird)
+
+    # Test when redshift is a scalar
+    z_scalar = z[0]
+    power_1d = linear_power[:, 0]
+    truth_scalar_redshift = truth_smith[:, 0]
+    smith_scalar_redshift = halofit_smith(k, z_scalar, power_1d, Planck15)
+    assert allclose(smith_scalar_redshift, truth_scalar_redshift)
+    assert np.shape(smith_scalar_redshift) == np.shape(power_1d)
+
+    # Test for failure when wavenumber is a scalar
+    k_scalar = k[0]
+    power_1d = linear_power[0, :]
+    with pytest.raises(TypeError):
+        halofit_smith(k_scalar, z, power_1d, Planck15)
+
+    # Test for failure when wavenumber array is the wrong size
+    k_wrong_size = np.logspace(-4.0, 2.0, 7)
+    with pytest.raises(ValueError):
+        halofit_smith(k_wrong_size, z, linear_power, Planck15)
+
+    # Test for failure when redshift arry is the wrong size
+    z_wrong_size = np.linspace(0.0, 2.0, 3)
+    with pytest.raises(TypeError):
+        halofit_smith(k, z_wrong_size, linear_power, Planck15)
+
+    # Test for failure when wavenumber is negative
+    k_negative = np.copy(k)
+    k_negative[0] = -1.0
+    with pytest.raises(ValueError):
+        halofit_smith(k_negative, z, linear_power, Planck15)
+
+    # Test for failure when wavenumber is zero
+    k_zero = np.copy(k)
+    k_zero[0] = 0.0
+    with pytest.raises(ValueError):
+        halofit_smith(k_zero, z, linear_power, Planck15)
+
+    # Test for failure when redshift is negative
+    z_negative = np.copy(z)
+    z_negative[0] = -1.0
+    with pytest.raises(ValueError):
+        halofit_smith(k, z_negative, linear_power, Planck15)
+
+    # Test for failure when linear power spectrum is negative
+    power_negative = np.copy(linear_power)
+    power_negative[0, 0] = -1.0
+    with pytest.raises(ValueError):
+        halofit_smith(k, z, power_negative, Planck15)
+
+    # Test for failure when wavenumber array is not in asscending order
+    k_wrong_order = k[::-1]
+    with pytest.raises(ValueError):
+        halofit_smith(k_wrong_order, z, linear_power, Planck15)
