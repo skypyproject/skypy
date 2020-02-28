103e1c5e86fd4dd1fccced1985eb0a2349b8021a
Richard R <rrjbca@users.noreply.github.com>
2020-02-28 11:40:38 +0000

    Use fit method from rv_continuous base class

--- skypy/galaxy/morphology.py
+++ skypy/galaxy/morphology.py
@@ -4,10 +4,10 @@ This module provides facilities to sample the morphological properties of
 galaxies using a number of models.
 """
 
-from scipy import stats
+from scipy.stats._continuous_distns import beta_gen
 
 
-def _kacprzak_input(beta_method):
+def _kacprzak_parameterization(beta_method):
     def reparameterize(self, *args):
         *beta_args, e_sum, e_ratio = args
         a = e_sum * e_ratio
@@ -16,40 +16,30 @@ def _kacprzak_input(beta_method):
     return reparameterize
 
 
-def _kacprzak_output(beta_method):
-    def reparameterize(self, *args, **kwds):
-        a, b, *beta_out = beta_method(self, *args, **kwds)
-        e_sum = a + b
-        e_ratio = a / (a + b)
-        return (e_sum, e_ratio, *beta_out)
-    return reparameterize
+class ellipticity_beta_gen(beta_gen):
 
-
-class ellipticity_beta_gen(stats._continuous_distns.beta_gen):
-
-    @_kacprzak_input
+    @_kacprzak_parameterization
     def _rvs(self, *args):
         return super()._rvs(*args)
 
-    @_kacprzak_input
+    @_kacprzak_parameterization
     def _logpdf(self, *args):
         return super()._logpdf(*args)
 
-    @_kacprzak_input
+    @_kacprzak_parameterization
     def _cdf(self, *args):
         return super()._cdf(*args)
 
-    @_kacprzak_input
+    @_kacprzak_parameterization
     def _ppf(self, *args):
         return super()._ppf(*args)
 
-    @_kacprzak_input
+    @_kacprzak_parameterization
     def _stats(self, *args):
         return super()._stats(*args)
 
-    @_kacprzak_output
     def fit(self, data, *args, **kwds):
-        return super().fit(data, *args, **kwds)
+        return super(beta_gen, self).fit(data, *args, **kwds)
 
 
 ellipticity_beta = ellipticity_beta_gen(a=0.0, b=1.0, name='ellipticity_beta')
