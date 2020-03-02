f86bf3d219fe5e3ec66fde456e66dba981972485
Richard R <rrjbca@users.noreply.github.com>
2020-03-02 13:14:46 +0000

    Docstring for ellipticity beta distribution

--- skypy/galaxy/morphology.py
+++ skypy/galaxy/morphology.py
@@ -17,6 +17,54 @@ def _kacprzak_parameterization(beta_method):
 
 
 class ellipticity_beta_gen(beta_gen):
+    r'''Galaxy ellipticities sampled from a reparameterized beta distribution.
+
+    The ellipticities follow a beta distribution parameterized by
+    :math:`e_{\rm sum}` and :math:`e_{\rm ratio}` as presented in Kacprzak et
+    al. 2019.
+
+    Parameters
+    ----------
+    e_sum : array_like
+        Parameter controlling the width of the distribution, must be positive.
+    e_ratio : array_like
+        Mean ellipticity of the distribution, must be between 0 and 1.
+
+    Notes
+    -----
+    The probability distribution function :math:`p(e)` for ellipticity :math:`e`
+    is given by a beta distribution:
+
+    .. math::
+
+        p(e) \sim \frac{\Gamma(\alpha+\beta)}{\Gamma(\alpha) \Gamma(\beta)}
+                  x^{\alpha-1} (1-x)^{\beta-1}
+
+    for :math:`0 <= e <= 1`, :math:`\alpha = e_{\rm sum} e_{\rm ratio}`,
+    :math:`\beta = e_{\rm sum} (1 - e_{\rm ratio})`, :math:`e_{\rm sum} > 0`
+    and :math:`0 < e_{\rm ratio} < 1`, where :math:`\Gamma` is the gamma
+    function.
+
+    References
+    ----------
+    [1] Kacprzak T., Herbel J., Nicola A. et al., arXiv:1906.01018
+
+    Examples
+    --------
+    >>> from skypy.galaxy.morphology import ellipticity_beta
+
+    Sample 10 random variates from the Kacprzak model with
+    :math:`e_{\rm sum} = 1.0` and :math:`e_{\rm ratio} = 0.5`:
+
+    >>> ellipticity = ellipticity_beta.rvs(1.0, 0.5, size=10)
+
+    Fix distribution parameters for repeated use:
+
+    >>> ellipticity_dist = ellipticity_beta(1.0, 0.5)
+    >>> ellipticity_dist.mean()
+    0.5
+    >>> ellipticity = ellipticity_dist.rvs(size=10)
+    '''
 
     @_kacprzak_parameterization
     def _rvs(self, *args):
