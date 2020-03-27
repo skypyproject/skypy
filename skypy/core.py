e369122e5f1503569691ff3643d696c4796ddd0e
Richard R <rrjbca@users.noreply.github.com>
2020-03-27 14:27:38 +0000

    Generate cosmology from config; refactor code

--- skypy/core.py
+++ skypy/core.py
@@ -1,45 +1,30 @@
-from astropy.cosmology import default_cosmology
 from astropy.table import Table
-
-
-def import_function(module, function):
-
-    fl = function.split('.')
-    module = __import__(module, fromlist=fl[0])
-    for f in fl:
-        module = getattr(module, f)
-    return module
 
 
 class SkyPyDriver:
 
-    def __init__(self):
-
-        self.cosmology = default_cosmology.get()
-
     def execute(self, config):
 
+        # Cosmology
+        self.cosmology = self.call_from_config(config['cosmology'])
+
+        # Tables
         for table, columns in config['tables'].items():
-            t = self.new_table(table)
-            for col, settings in columns.items():
-                module_name = settings['module']
-                function_name = settings['function']
-                function = import_function(module_name, function_name)
-                args = settings.get('args', [])
-                kwargs = settings.get('kwargs', {})
-                req = settings.get('requires', {})
-                req = {k: self.get(v) for k, v in req.items()}
-                t[col] = function(*args, **kwargs, **req)
+            setattr(self, table, Table())
+            for column, settings in columns.items():
+                getattr(self, table)[column] = self.call_from_config(settings)
 
-    def new_table(self, name):
+    def call_from_config(self, settings):
 
-        setattr(self, name, Table())
-        return getattr(self, name)
+        # Import function
+        module = __import__(settings['module'], fromlist=settings['function'])
+        function = getattr(module, settings['function'])
 
-    def get(self, name):
+        # Parse arguments
+        args = settings.get('args', [])
+        kwargs = settings.get('kwargs', {})
+        req = settings.get('requires', {})
+        req = {k: getattr(self, v) for k, v in req.items()}
 
-        attribute, *key = name.split('.')
-        attribute = getattr(self, attribute)
-        if type(attribute) == Table:
-            attribute = attribute[key]
-        return attribute
+        # Call function
+        return function(*args, **kwargs, **req)
