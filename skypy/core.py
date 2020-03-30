0702d747739309a906f1ff953d80171e6b4d000f
Richard R <rrjbca@users.noreply.github.com>
2020-03-30 12:14:03 +0100

    Use config get method with default return values

--- skypy/core.py
+++ skypy/core.py
@@ -6,24 +6,26 @@ class SkyPyDriver:
     def execute(self, config):
 
         # Cosmology
-        self.cosmology = self.call_from_config(config['cosmology'])
+        self.cosmology = self.call_from_config(config.get('cosmology'))
 
         # Tables
-        for table, columns in config['tables'].items():
+        for table, columns in config.get('tables', {}).items():
             setattr(self, table, Table())
             for column, settings in columns.items():
                 getattr(self, table)[column] = self.call_from_config(settings)
 
-    def call_from_config(self, settings):
+    def call_from_config(self, config):
 
         # Import function
-        module = __import__(settings['module'], fromlist=settings['function'])
-        function = getattr(module, settings['function'])
+        module_name = config.get('module')
+        function_name = config.get('function')
+        module = __import__(module_name, fromlist=function_name)
+        function = getattr(module, function_name)
 
         # Parse arguments
-        args = settings.get('args', [])
-        kwargs = settings.get('kwargs', {})
-        req = settings.get('requires', {})
+        args = config.get('args', [])
+        kwargs = config.get('kwargs', {})
+        req = config.get('requires', {})
         req = {k: getattr(self, v) for k, v in req.items()}
 
         # Call function
