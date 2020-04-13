a6bedf54076640fc902e56c00408ade69e8c5bc2
Richard R <rrjbca@users.noreply.github.com>
2020-04-13 16:58:07 +0100

    Documentation for the new pipeline module including SkyPyDriver

--- skypy/pipeline/driver.py
+++ skypy/pipeline/driver.py
@@ -1,11 +1,74 @@
+"""Driver module.
+
+This module provides methods to run pipelines of functions with dependencies
+and handle their results.
+
+.. autosummary::
+   :nosignatures:
+   :toctree: ../api/
+
+   SkyPyDriver
+
+"""
+
 from astropy.table import Table
 import networkx
 import re
 
 
 class SkyPyDriver:
+    r'''Class for running pipelines.
+
+    This is the main class for running pipelines of functions with dependencies
+    and using their results to generate tables.
+    '''
 
     def execute(self, config, file_format=None):
+        r'''Run a pipeline.
+
+        This function runs a pipeline of functions to generate the cosmology
+        and the columns of a set of tables. It uses a Directed Acyclic Graph to
+        determine a non-blocking order of execution that resolves any
+        dependencies, see [1]_. Tables can optionally be written to file.
+
+        Parameters
+        ----------
+        config : dict-like
+            Configuration for the pipeline.
+        file_format : str
+            File format used to write tables. Files are written using the
+            Astropy unified file read/write interface; see [2]_ for supported
+            file formats. If None (default) tables are not written to file.
+
+        Notes
+        -----
+        Each step in the pipeline is configured by a dictionary specifying:
+
+        - 'function' : the name of the function
+        - 'module' : the name of the the module to import 'function' from
+        - 'args' : a list of positional arguments (by value)
+        - 'kwargs' : a dictionary of keyword arguments
+        - 'requires' : a dictionary of keyword arguments
+
+        Note that 'kwargs' specifices keyword arguments by value, wheras
+        'requires' specifices the names of previous steps in the pipeline and
+        uses their return values as keyword arguments.
+
+        'config' should contain 'cosmology' and/or 'tables'. 'cosmology' should
+        return a dictionary configuring a function that returns an
+        astropy.cosmology.Cosmology object. 'tables' should contain a set of
+        nested dictionaries, first giving the names of each table, then the
+        names of each column within each table. Each column should return a
+        dictionary configuring a function that returns an array-like object.
+
+        See [3]_ for examples of pipeline configurations in yaml format.
+
+        References
+        ----------
+        .. [1] https://networkx.github.io/documentation/stable/
+        .. [2] https://docs.astropy.org/en/stable/io/unified.html
+        .. [3] https://github.com/skypyproject/skypy/tree/master/examples
+        '''
 
         # Create a Directed Acyclic Graph of all jobs and dependencies
         dag = networkx.DiGraph()
