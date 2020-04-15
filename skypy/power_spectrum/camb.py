35c47a723079f3057753a09f9c74e5f134d89c3f
Ian Harrison <itrharrison@gmail.com>
2020-04-15 17:25:08 +0100

    updated power spectrum naming convention (#116)

--- skypy/power_spectrum/camb.py
+++ skypy/power_spectrum/camb.py
@@ -11,7 +11,7 @@ Models
    :nosignatures:
    :toctree: ../api/
 
-   camb
+   power_spectrum
 
 """
 
@@ -19,7 +19,7 @@ import numpy as np
 from astropy import units as u
 
 
-def camb(wavenumber, redshift, cosmology, A_s, n_s):
+def power_spectrum(wavenumber, redshift, cosmology, A_s, n_s):
     """ Return the CAMB computation of the linear matter power spectrum, on a
     two dimensional grid of wavenumber and redshift
 
@@ -58,7 +58,8 @@ def camb(wavenumber, redshift, cosmology, A_s, n_s):
     >>> wavenumber = np.array([1.e-2, 1.e-1, 1e0])
     >>> A_s = 2.e-9
     >>> n_s = 0.965
-    >>> camb(wavenumber, redshift, cosmology, A_s, n_s)  # doctest: +SKIP
+    >>> power_spectrum(wavenumber, redshift, \
+    ...                cosmology, A_s, n_s)  # doctest: +SKIP
     array([[2.34758952e+04, 8.70837957e+03],
            [3.03660813e+03, 1.12836115e+03],
            [2.53124880e+01, 9.40802814e+00]])
--- skypy/power_spectrum/halofit.py
+++ skypy/power_spectrum/halofit.py
@@ -11,11 +11,11 @@ Models
    :nosignatures:
    :toctree: ../api/
 
-   halofit
+   power_spectrum_nl
    HalofitParameters
-   halofit_smith
-   halofit_takahashi
-   halofit_bird
+   power_spectrum_nl_smith
+   power_spectrum_nl_takahashi
+   power_spectrum_nl_bird
 
 """
 
@@ -72,8 +72,8 @@ _bird_parameters = HalofitParameters(
     2.080, 1.2e-3, 26.3, -6.49, 1.44, 12.4)
 
 
-def halofit(wavenumber, redshift, linear_power_spectrum,
-            cosmology, parameters):
+def power_spectrum_nl(wavenumber, redshift, linear_power_spectrum,
+                      cosmology, parameters):
     r'''Computation of the non-linear halo power spectrum.
 
     This function computes the non-linear halo power spectrum, as a function
@@ -115,7 +115,7 @@ def halofit(wavenumber, redshift, linear_power_spectrum,
     >>> zvalue = 0.0
     >>> pvec = np.array([388.6725682632502, 0.21676249605280398])
     >>> cosmo = FlatLambdaCDM(H0=67.04, Om0=0.21479, Ob0=0.04895)
-    >>> halofit(kvec, zvalue, pvec, cosmo, _takahashi_parameters)
+    >>> power_spectrum_nl(kvec, zvalue, pvec, cosmo, _takahashi_parameters)
     array([388.67064424,   0.72797614])
     '''
 
@@ -233,6 +233,9 @@ def halofit(wavenumber, redshift, linear_power_spectrum,
     return pknl.T.reshape(return_shape)
 
 
-halofit_smith = partial(halofit, parameters=_smith_parameters)
-halofit_takahashi = partial(halofit, parameters=_takahashi_parameters)
-halofit_bird = partial(halofit, parameters=_bird_parameters)
+power_spectrum_nl_smith = partial(power_spectrum_nl,
+                                  parameters=_smith_parameters)
+power_spectrum_nl_takahashi = partial(power_spectrum_nl,
+                                      parameters=_takahashi_parameters)
+power_spectrum_nl_bird = partial(power_spectrum_nl,
+                                 parameters=_bird_parameters)
