a2fe945298fb22ef1dd9df14da32f3a346af9c10
Ian Harry <ian.harry@ligo.org>
2020-04-15 17:17:32 +0100

    Use skypy image in docs build (#121)

--- setup.py
+++ setup.py
@@ -12,6 +12,11 @@ from setuptools.config import read_configuration
 
 from astropy_helpers.setup_helpers import register_commands, get_package_info
 from astropy_helpers.version_helpers import generate_version_py
+try:
+    from astropy_helpers.commands.build_sphinx import AstropyBuildDocs
+    sphinx_present = True
+except ImportError:
+    sphinx_present = False
 
 # Store the package name in a built-in variable so it's easy
 # to get from other parts of the setup infrastructure
@@ -20,6 +25,19 @@ builtins._ASTROPY_PACKAGE_NAME_ = read_configuration('setup.cfg')['metadata']['n
 # Create a dictionary with setup command overrides. Note that this gets
 # information about the package (name and version) from the setup.cfg file.
 cmdclass = register_commands()
+
+if sphinx_present:
+    class build_docs(AstropyBuildDocs):
+        def run(self):
+            import shutil
+            super().run()
+            shutil.copy('images/skypy_image.png',
+                        './docs/_build/html/_static/astropy_logo_32.png')
+            shutil.copy('images/skypy_image.svg',
+                        './docs/_build/html/_static/astropy_logo.svg')
+
+
+    cmdclass['build_docs'] = build_docs
 
 # Freeze build information in version.py. Note that this gets information
 # about the package (name and version) from the setup.cfg file.
