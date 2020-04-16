b59ac2217ea62e7f70d03e55331e8592a426af7c
Lucia F. de la Bella <Lucia-Fonseca@users.noreply.github.com>
2020-04-16 17:12:11 +0100

    Shaffling comments order

--- skypy/__init__.py
+++ skypy/__init__.py
@@ -1,13 +1,13 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
 """
 Skypy is a package offering core functionality and common tools for
 astronomical forward-modelling in Python. It contains methods for modelling
 the Universe, galaxies and the Milky Way and for generating synthetic
 observational data.
 """
+
+# Packages may add whatever they like to this file, but
+# should keep this content at the top.
 # ----------------------------------------------------------------------------
 from ._astropy_init import *   # noqa
 # ----------------------------------------------------------------------------
