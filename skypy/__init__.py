fc4c990370f1a66aa8deea8a3a28388b7ed6a78e
Lucia F. de la Bella <Lucia-Fonseca@users.noreply.github.com>
2020-04-16 16:38:45 +0100

    Fixing documentation

--- skypy/__init__.py
+++ skypy/__init__.py
@@ -1,7 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Skypy is a package offering core functionality and common tools for
+astronomical forward-modelling in Python. It contains methods for modelling
+the Universe, galaxies and the Milky Way and for generating synthetic
+observational data.
+"""
 
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
 # ----------------------------------------------------------------------------
 from ._astropy_init import *   # noqa
 # ----------------------------------------------------------------------------
@@ -26,6 +30,7 @@ if LooseVersion(sys.version) < LooseVersion(__minimum_python_version__):
 
 if not _ASTROPY_SETUP_:   # noqa
     # For egg_info test builds to pass, put package imports here.
+    from .example_mod import *   # noqa
     # Then you can be explicit to control what ends up in the namespace,
     __all__ += ['do_primes']   # noqa
     # or you can keep everything from the subpackage with the following instead
