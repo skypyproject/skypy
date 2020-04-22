3f87acad6b5696aafaef4dd64d00baa24be61c82
Richard R <rrjbca@users.noreply.github.com>
2020-04-22 12:51:33 +0100

    Create argument parser inside main function

--- skypy/pipeline/scripts/skypy.py
+++ skypy/pipeline/scripts/skypy.py
@@ -39,14 +39,14 @@ from skypy.pipeline.driver import SkyPyDriver
 import sys
 import yaml
 
-parser = argparse.ArgumentParser(description="SkyPy pipeline driver")
-parser.add_argument('-c', '--config', required=True,
-                    type=argparse.FileType('r'), help='Config file name')
-parser.add_argument('-f', '--format', required=False,
-                    choices=['fits', 'hdf5'], help='Table file format')
-
 
 def main(args=None):
+
+    parser = argparse.ArgumentParser(description="SkyPy pipeline driver")
+    parser.add_argument('-c', '--config', required=True,
+                        type=argparse.FileType('r'), help='Config file name')
+    parser.add_argument('-f', '--format', required=False,
+                        choices=['fits', 'hdf5'], help='Table file format')
 
     args = parser.parse_args(args or sys.argv[1:] or ['--help'])
     config = yaml.safe_load(args.config)
