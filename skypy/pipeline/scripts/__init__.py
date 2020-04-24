b4bef26b8c5fae046428090ce1aa3d317da4e99d
Richard R <58728519+rrjbca@users.noreply.github.com>
2020-04-24 14:47:41 +0100

    skypy.pipeline.scripts docstring

--- skypy/pipeline/scripts/__init__.py
+++ skypy/pipeline/scripts/__init__.py
@@ -1,5 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
-This subpackage contains implementations of command-line scripts that are
-included with Astropy.
+This subpackage implements the ``skypy`` command-line script for running
+end-to-end pipelines.
 """
