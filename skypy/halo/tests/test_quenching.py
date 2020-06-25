b62b254b0b6323f31182ae7b15e4c625a03d1570
Lucia F. de la Bella <Lucia-Fonseca@users.noreply.github.com>
2020-06-25 14:25:31 +0100

    Minor changes in test

--- skypy/halo/tests/test_quenching.py
+++ skypy/halo/tests/test_quenching.py
@@ -3,34 +3,28 @@ from scipy import stats
 from collections import Counter
 
 
-import skypy.halo.quenching as q
+from skypy.halo.quenching import environment_quenched, mass_quenched
 
 
-def test_environment_quenching():
-    # Test the environment quenching function for 1000 subhalos
-    number_subhalos = 1000
-    quenched = q.environment_quenching(number_subhalos)
-
-    # number of quenched subhalos
+def test_environment_quenched():
+    # Test the quenching process follows a binomial distribution
+    n, p = 1000, 0.7
+    quenched = environment_quenched(n, p)
     number_quenched = Counter(quenched)[0]
 
-    # Test the quenching process follows a binomial distribution
-    p_value = stats.binom_test(number_quenched, n=number_subhalos, p=0.5,
+    p_value = stats.binom_test(number_quenched, n=n, p=p,
                                alternative='greater')
     assert p_value > 0.05
 
 
-def test_mass_quenching():
-    # Test the mass quenching function for 1000 halos
-    number_halos = 1000
-    offset, width = 12, 6
-    halo_mass = np.linspace(0, 24, num=number_halos)
-    quenched = q.mass_quenching(halo_mass, offset, width)
-
-    # number of quenched halos
+def test_mass_quenched():
+    # Test the quenching process follows a binomial distribution
+    n = 1000
+    offset, width = 1.0e12, 0.5
+    halo_mass = 10 ** np.random.uniform(11, 13, n)
+    quenched = mass_quenched(halo_mass, offset, width)
     number_quenched = Counter(quenched)[0]
 
-    # Test the quenching process follows a binomial distribution
-    p_value = stats.binom_test(number_quenched, n=number_halos, p=0.5,
+    p_value = stats.binom_test(number_quenched, n=n, p=0.5,
                                alternative='greater')
     assert p_value > 0.05
