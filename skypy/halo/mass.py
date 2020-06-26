8f35faea8f2fcadaa8a9233a02407ea3c5176e9e
Lucia F. de la Bella <Lucia-Fonseca@users.noreply.github.com>
2020-06-26 12:07:39 +0100

    Major structural changes

--- skypy/halo/mass.py
+++ skypy/halo/mass.py
@@ -7,23 +7,200 @@ Models
    :nosignatures:
    :toctree: ../api/
    press_schechter
-   Press_Schechter_sampler
-   Sheth_Tormen_sampler
+   halo_mass_function
+   sheth_tormen_collapse_function
+   press_schechter_collapse_function
 '''
 
 import numpy as np
 from scipy import integrate
 from functools import partial
 from astropy.constants import G
+from astropy import units as u
 
+from skypy.power_spectrum import growth_function
 from skypy.utils.random import schechter
 
 __all__ = [
     'press_schechter',
     'halo_mass_function',
-    'Press_Schechter_sampler',
-    'Sheth_Tormen_sampler',
+    'sheth_tormen_collapse_function',
+    'press_schechter_collapse_function',
  ]
+
+
+def _sigma_squared(M, wavenumber, linear_power_today, redshift, cosmology):
+    k = wavenumber
+    Pk = linear_power_today
+    Hz = cosmology.H(redshift)
+
+    R = np.power(2 * G.value * M / Hz, 1.0 / 3.0)
+    j1 = (np.sin(k * R) - np.cos(k * R) * k * R) / (k * R)
+    top_hat = 3 * j1 / (k * R)**2
+    integrand = Pk * top_hat**2.0 * k**2.0
+    return integrate.simps(integrand, k) / (2. * np.pi ** 2.)
+
+
+def halo_mass_function(collapse_function, m_min, m_max, m_star, redshift,
+                       wavenumber, power_spectrum, cosmology,
+                       resolution=100, size=None):
+    """Halo mass function.
+    This function samples haloes from their mass function following Sheth &
+    Tormen formalism, see equation 7.46 in [1]_.
+
+    Parameters
+    -----------
+    collapse_function: function
+        Collapse function to choose from a variety of models:
+        `sheth_tormen_collapse_function`, `press_schechter_collapse_function`.
+    m_min, m_max : (nm,) array_like
+        Lower and upper bounds for the random variable `m`.
+    m_star: float
+        Factors parameterising the characteristic mass.
+    redshift : float
+        Redshift value at which to evaluate the variance of the power spectrum.
+    wavenumber : (nk,) array_like
+        Array of wavenumbers at which the power spectrum is evaluated,
+        in units of :math:`[Mpc^-1]`.
+    power_spectrum: (nk,) array_like
+        Linear power spectrum at redshift 0 in :math:`[Mpc^3]`.
+    cosmology : astropy.cosmology.Cosmology
+        Cosmology object providing methods for the evolution history of
+        omega_matter and omega_lambda with redshift.
+    resolution : int, optional
+        Resolution of the inverse transform sampling spline. Default is 100.
+    size: int, optional
+        Output shape of samples. Default is None.
+
+    Returns
+    --------
+    sample: array_like
+        Samples drawn from the Sheth-Tormen function, equation (10) in [1].
+
+    Examples
+    ---------
+    >>> import numpy as np
+    >>> from skypy.halo import mass
+
+    Note here that the variable :math:`nu` that appears in the Sheth and Tormen
+    collapse function is the squared version of the variable :math:`nu` in
+    equation 7.46 in [1].
+
+    References
+    ----------
+    .. [1] Mo, H. and van den Bosch, F. and White, S. (2010), Cambridge
+        University Press, ISBN: 9780521857932.
+    """
+    f_c = collapse_function
+    k = wavenumber
+    Pk = power_spectrum
+
+    # The linear frowth function
+    D0 = growth_function(0, cosmology)
+    Dz = growth_function(redshift, cosmology) / D0
+
+    # nu as a function of mass for a given redshift
+    m = np.logspace(np.log10(np.min(m_min)), np.log10(np.max(m_max)),
+                    resolution)
+    delta_critical_squared = _sigma_squared(m_star)
+    nu = np.sqrt(delta_critical_squared / _sigma_squared(m)) / Dz
+
+    # Rest of prefactors
+    rho_bar = cosmology.critical_density0 * (cosmology.efunc(redshift)) ** 2
+    rho_bar = (rho_bar.to(u.kg / u.m ** 3)).value
+    dlognu_dlogm = nu * Pk * k  # work on this. This is nothing now
+
+    # Sampling from the halo mass function
+    PDF = rho_bar * f_c * dlognu_dlogm / np.power(m, 2)
+    CDF = integrate.cumtrapz(PDF, m, initial=0)
+    cdf = CDF/CDF[-1]
+    t_lower = np.interp(np.min(m), m, cdf)
+    t_upper = np.interp(np.max(m), m, cdf)
+    n_uniform = np.random.uniform(t_lower, t_upper, size=size)
+    n_sample = np.interp(n_uniform, cdf, m)
+
+    return n_sample
+
+
+def sheth_tormen_collapse_function(x, A, a, p):
+    """Sheth & Tormen collapse fraction.
+    This function computes the Sheth & Tormen mass fumction for ellipsoidal
+    collapse, see equation 10 in [1]_ or [2]_.
+
+    Parameters
+    -----------
+    x: (nx,) array_like ## work on this
+        Array of the random variable :math:`\nu` appearing in equation 10 [1].
+    A,a,p: float
+        Parameters of the Sheth-Tormen formalism.
+
+    Returns
+    --------
+    f_sp: array_like
+        Array with the values of the collapse function.
+
+    Examples
+    ---------
+    >>> import numpy as np
+
+    References
+    ----------
+    .. [1] R. K. Sheth and G. Tormen,  Mon. Not. Roy. Astron. Soc. 308, 119
+        (1999), astro-ph/9901122.
+    .. [2] https://www.slac.stanford.edu/econf/C070730/talks/
+        Wechsler_080207.pdf
+    """
+
+    return (A / (np.sqrt(np.pi) * x)) * (1.0 + 1.0 / (a * x)**p) *\
+        np.sqrt(a * x / 2.0) * np.exp(-(a * x / 2.0))
+
+
+press_schechter_collapse_function = partial(sheth_tormen_collapse_function,
+                                            0.5, 1, 0)
+
+
+def _derivative(f, a, method='central', step=0.01):
+    r'''Numerical derivative.
+    This function computes the difference formula for f'(a) with step size h.
+
+    Parameters
+    ----------
+    f : function
+        Vectorized function of one variable.
+    a : number
+        Compute derivative at :math:`x = a`.
+    method : string
+        Difference formula: 'forward', 'backward' or 'central'.
+    step : number
+        Step size in difference formula.
+
+    Returns
+    -------
+    float
+
+    Notes
+    -----
+    The difference formula:
+        central: :math:`(f(a+h) - f(a-h))/2h`
+        forward: :math:`(f(a+h) - f(a))/h`
+        backward: :math:`(f(a) - f(a-h))/h`
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> _derivative(np.cos, 0, method='forward', step=1e-8)
+    0.0
+    '''
+    h = step
+
+    if method == 'central':
+        return (f(a + h) - f(a - h)) / (2 * h)
+    elif method == 'forward':
+        return (f(a + h) - f(a)) / h
+    elif method == 'backward':
+        return (f(a) - f(a - h)) / h
+    else:
+        raise ValueError("Method must be 'central', 'forward' or 'backward'.")
 
 
 def press_schechter(n, m_star, size=None, x_min=0.00305,
@@ -32,7 +209,7 @@ def press_schechter(n, m_star, size=None, x_min=0.00305,
     """Sampling from Press-Schechter mass function (1974).
 
     Masses following the Press-Schechter mass function following the
-    Press et al. [1]_ formalism.
+    Press and Schechter [1]_ formalism.
     Parameters
     ----------
     n : float or int
@@ -67,87 +244,3 @@ def press_schechter(n, m_star, size=None, x_min=0.00305,
     x_sample = schechter(alpha, x_min, x_max, resolution=resolution, size=size)
 
     return m_star * np.power(x_sample, 3.0 / (n + 3.0))
-
-
-def halo_mass_function(A, a, p, m_min, m_max, m_star, redshift,
-                       power_spectrum, wavenumber, cosmology,
-                       resolution=100, size=None):
-    """Peak height.
-    This function samples haloes from their mass function following Sheth &
-    Tormen formalism, see equation 10 in [1]_ or [2]_.
-
-    Parameters
-    -----------
-    A,a,p: float
-        Parameters
-    m_min, m_max : array_like
-        Lower and upper bounds for the random variable m.
-    m_star: float or int
-        Factors parameterising the characteristic mass.
-    redshift : float
-        Redshift value at which to evaluate the variance of the power spectrum.
-    power_spectrum: (nk,) array_like
-        Linear power spectrum at a single redshift in [Mpc^3].
-        The first axis correspond to k values in [Mpc^-1].
-    wavenumber : (nk,) array_like
-        Array of wavenumbers at which the power spectrum is evaluated,
-        in units of [Mpc^-1].
-    cosmology : astropy.cosmology.Cosmology
-        Cosmology object providing methods for the evolution history of
-        omega_matter and omega_lambda with redshift.
-    resolution : int, optional
-        Resolution of the inverse transform sampling spline. Default is 100.
-    size: int, optional
-        Output shape of samples. Default is None.
-
-    Returns
-    --------
-    sample: array_like
-        Samples drawn from the Sheth-Tormen function, equation (10) in [1].
-
-    Examples
-    ---------
-    >>> import numpy as np
-
-    References
-    ----------
-    .. [1] R. K. Sheth and G. Tormen,  Mon. Not. Roy. Astron. Soc. 308, 119
-        (1999), astro-ph/9901122.
-    .. [2] https://www.slac.stanford.edu/econf/C070730/talks/
-        Wechsler_080207.pdf
-    """
-    k = wavenumber
-    Pk = power_spectrum
-    Hz = cosmology.H(redshift)
-
-    def sigma_squared(M):
-        R = np.power(2 * G.value * M / Hz, 1.0 / 3.0)
-        j1 = (np.sin(k * R) - np.cos(k * R) * k * R) / (k * R)
-        top_hat = 3 * j1 / (k * R)**2
-        integrand = Pk * top_hat**2.0 * k**2.0
-        return integrate.simps(integrand, k) / (2. * np.pi ** 2.)
-
-    delta_critical_squared = sigma_squared(m_star)
-
-    m = np.logspace(np.log10(np.min(m_min)), np.log10(np.max(m_max)),
-                    resolution)
-    nu = delta_critical_squared / sigma_squared(m)
-
-    cdf = _sheth_tormen_cdf(nu, A, a, p)
-    t_lower = np.interp(np.min(nu), nu, cdf)
-    t_upper = np.interp(np.max(nu), nu, cdf)
-    nu_uniform = np.random.uniform(t_lower, t_upper, size=size)
-    nu_sample = np.interp(nu_uniform, cdf, nu)
-
-    return nu_sample
-
-
-def _sheth_tormen_cdf(x, A, a, p):
-    PDF = (A / (np.sqrt(np.pi) * x)) * (1.0 + 1.0 / (a * x)**p) *\
-        np.sqrt(a * x / 2.0) * np.exp(-(a * x / 2.0))
-    CDF = integrate.cumtrapz(PDF, x, initial=0)
-    return CDF/CDF[-1]
-
-
-Press_Schechter_sampler = partial(halo_mass_function, 0.5, 1, 0)
-Sheth_Tormen_sampler = partial(halo_mass_function, 0.3222, 0.707, 0.3)
