69888aeec9028cc2485f9fa3aee8bae85bbf5984
Ian Harrison <itrharrison@gmail.com>
2020-06-30 15:58:53 +0100

    PEP8 fixes

--- skypy/gravitational_wave/tests/test_merger_rate.py
+++ skypy/gravitational_wave/tests/test_merger_rate.py
@@ -1,13 +1,14 @@
 import skypy.galaxy.luminosity as lum
 import skypy.gravitational_waves.merger_rate as merg
 
+
 def test_abadie_rates():
 
-  luminosities = lum.herbel_luminosities(1.0, -1.3, -0.9408582,
-                                         -20.40492365, size=100)
+    luminosities = lum.herbel_luminosities(1.0, -1.3, -0.9408582,
+                                           -20.40492365, size=100)
 
-  abIII_rates = merg.abadie_tableIII_merger_rates(luminosities,
-                                                  population='NS-NS',
-                                                  optimism='low')
+    abIII_rates = merg.abadie_tableIII_merger_rates(luminosities,
+                                                    population='NS-NS',
+                                                    optimism='low')
 
-  assert len(abIII_rates)==len(luminosities)
+    assert len(abIII_rates) == len(luminosities)
