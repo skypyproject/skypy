1977fe18d39a8095e402b99b855ab686d686839f
Nicolas Tessore <nicolas.tessore@manchester.ac.uk>
2020-08-06 20:47:46 +0100

    test Schechter special case of gamma distribution

--- skypy/utils/tests/test_random.py
+++ skypy/utils/tests/test_random.py
@@ -22,7 +22,7 @@ def test_schechter():
     x_min = 1e-10
     x_max = 1e2
 
-    x = schechter(alpha, x_min=x_min, x_max=x_max, resolution=100, size=1000)
+    x = schechter(alpha, x_min=x_min, x_max=x_max, size=1000)
 
     assert np.all(x >= x_min)
     assert np.all(x <= x_max)
@@ -30,4 +30,19 @@ def test_schechter():
     # Test the distribution of galaxy properties follows the right distribution
     cdf = schechter_cdf_gen(alpha, x_min, x_max)
     _, p = kstest(x, cdf)
+    assert p > 0.01
+
+
+def test_schechter_gamma():
+
+    from skypy.utils.random import schechter
+
+    # when alpha > -1, x_min ≈ 0, x_max ≈ ∞, distribution is gamma
+    alpha = np.random.uniform(-1, 2)
+    x_min = 1e-10
+    x_max = 1e+10
+
+    x = schechter(alpha, x_min, x_max, resolution=100000, size=1000)
+
+    _, p = kstest(x, 'gamma', args=(alpha+1,))
     assert p > 0.01
