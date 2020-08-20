c1e0d32bde2a37a56de37d915e492c28d5f91e4d
Richard R <58728519+rrjbca@users.noreply.github.com>
2020-08-20 12:54:29 +0100

    Update skypy/power_spectrum/tests/test_camb.py

--- skypy/power_spectrum/tests/test_camb.py
+++ skypy/power_spectrum/tests/test_camb.py
@@ -6,7 +6,7 @@ import pytest
 
 # load the external camb result to test against
 camb_result_filename = get_pkg_data_filename('data/camb_result.txt')
-test_pkz = np.loadtxt(camb_result_filename, delimiter=',').T
+test_pkz = np.loadtxt(camb_result_filename, delimiter=',')
 
 # try to import the requirement, if it doesn't exist, skip test
 try:
