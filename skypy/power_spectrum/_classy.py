b54d2732dc13bf6d968e66e1506af027b994b107
Ian Harrison <itrharrison@gmail.com>
2020-08-25 10:40:48 +0100

    tidied up test

--- skypy/power_spectrum/_classy.py
+++ skypy/power_spectrum/_classy.py
@@ -1,6 +1,5 @@
 import numpy as np
 from astropy import units as u
-import pdb
 
 __all__ = [
     'classy',
--- skypy/power_spectrum/tests/test_class.py
+++ skypy/power_spectrum/tests/test_class.py
@@ -2,34 +2,22 @@ import numpy as np
 from astropy.cosmology import Planck15
 from astropy.units import allclose
 from astropy.utils.data import get_pkg_data_filename
-from unittest.mock import patch, MagicMock
 import pytest
 
 # load the external class result to test against
 class_result_filename = get_pkg_data_filename('data/class_result.txt')
 test_pkz = np.loadtxt(class_result_filename, delimiter=',')
 
-values = {'0': test_pkz[:,0], '1': test_pkz[:,0]}
-def side_effect(arg1, arg2):
-    return values[str(arg)]
-
-# create a mock object and specify values for all the attributes needed in
-# _class.py
-class_mock = MagicMock()
-class_mock.Class().set.return_value = 1
-class_mock.Class().compute.return_value = 1
-#class_mock.Class().pk = test_pkz_list.pop
-class_mock.Class().pk = MagicMock(side_effect=side_effect)
-
-# try to import the requirement, if it doesn't exist, use the mock instead
+# try to import the requirement, if it doesn't exist, skip test
 try:
-    __import__('classyc')
-    class_import_loc = {}
+    __import__('classylss')
 except ImportError:
-    class_import_loc = {'classyc': class_mock}
+    CLASS_NOT_FOUND = True
+else:
+    CLASS_NOT_FOUND = False
 
 
-@patch.dict('sys.modules', class_import_loc)
+@pytest.mark.skipif(CLASS_NOT_FOUND, reason='classylss not found')
 def test_classy():
     '''
     Test a default astropy cosmology
