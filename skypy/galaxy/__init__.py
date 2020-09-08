b5255c6759369710172fbe4bb2591d10cdcabb27
Richard R <rrjbca@users.noreply.github.com>
2020-09-08 15:17:42 +0100

    Import packages and modules

--- skypy/galaxy/__init__.py
+++ skypy/galaxy/__init__.py
@@ -2,3 +2,10 @@
 This module contains methods that model the intrinsic properties of galaxy
 populations.
 """
+
+from .ellipticity import *  # noqa F401,F403
+from .luminosity import *  # noqa F401,F403
+from .redshift import *  # noqa F401,F403
+from .size import *  # noqa F401,F403
+from .spectrum import *  # noqa F401,F403
+from .stellar_mass import *  # noqa F401,F403
--- skypy/halo/__init__.py
+++ skypy/halo/__init__.py
@@ -2,3 +2,7 @@
 This module contains methods that model the properties of dark matter halo
 populations.
 """
+
+from .abundance_matching import *  # noqa F401,F403
+from .mass import *  # noqa F401,F403
+from .quenching import *  # noqa F401,F403
