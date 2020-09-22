a8226f980c4392ecd74cf0f84a35332e34d890ac
Sarah <github@sarahbridle.net>
2020-09-22 16:59:57 +0100

    Placeholder for halo circuloar velocity function

---
+++ skypy/halo/properties.py
@@ -0,0 +1,23 @@
+"""Halo properties module.
+
+This module provides methods to add simple properties to halos
+
+Models
+======
+
+.. autosummary::
+   :nosignatures:
+   :toctree: ../api/
+
+   halo_circular_velocity
+
+"""
+
+
+
+def halo_circular_velocity():
+    """stuff here
+        """
+
+    #halos.sort(mass, reverse=True)
+    
