bfdfed92b3f4b1315578376a184cc71373e80f26
Nicolas Tessore <nicolas.tessore@manchester.ac.uk>
2020-09-23 11:36:32 +0100

    much simpler implementation

--- skypy/position/_uniform.py
+++ skypy/position/_uniform.py
@@ -1,10 +1,7 @@
 '''Implementations of uniform distributions.'''
 
 import numpy as np
-
 from astropy import units
-from astropy.coordinates import SkyCoord, SkyOffsetFrame
-
 
 TWO_PI = 2*np.pi
 PI_HALF = np.pi/2
@@ -39,21 +36,12 @@ def uniform_around(centre, area, size):
 
     '''
 
-    # get cosine of the opening angle from the area
-    cos_alpha = 1 - area.to_value(units.sr)/TWO_PI
+    # get cosine of maximum separation from area
+    cos_theta_max = 1 - area.to_value(units.sr)/TWO_PI
 
-    # randomly sample points within opening angle
-    cos_theta = np.random.uniform(cos_alpha, 1, size=size)
-    sin_theta = np.sqrt(1 - cos_theta**2)
+    # randomly sample points within separation
+    theta = np.arccos(np.random.uniform(cos_theta_max, 1, size=size))
     phi = np.random.uniform(0, TWO_PI, size=size)
 
-    # rotate to longitude, latitude
-    sin_sin, sin_cos = sin_theta*np.sin(phi), sin_theta*np.cos(phi)
-    lon = np.arctan2(sin_sin, cos_theta)
-    lat = PI_HALF - np.arctan2(np.sqrt(1 - sin_cos**2), -sin_cos)
-
     # construct random sky coordinates around centre
-    coords = SkyCoord(lon, lat, unit=units.rad, frame=SkyOffsetFrame(origin=centre))
-
-    # return converted to input frame
-    return coords.transform_to(centre, merge_attributes=False)
+    return centre.directional_offset_by(phi, theta)
