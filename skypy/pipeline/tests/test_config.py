7c19cf4853c2be40a7aed7b25e49d25e558351da
Richard R <rrjbca@users.noreply.github.com>
2020-10-01 09:03:01 +0100

    Test that parsed config entries have the correct types

---
+++ skypy/pipeline/tests/test_config.py
@@ -0,0 +1,19 @@
+from astropy.utils.data import get_pkg_data_filename
+from skypy.pipeline import skypy_config
+
+
+def test_config():
+
+    # Read empty config file
+    filename = get_pkg_data_filename('data/empty_config.yml')
+    assert skypy_config(filename) == {}
+
+    # Read config file and check entries are parsed to the correct types
+    filename = get_pkg_data_filename('data/test_config.yml')
+    config = skypy_config(filename)
+    assert isinstance(config['test_int'], int)
+    assert isinstance(config['test_float'], float)
+    assert isinstance(config['test_str'], str)
+    assert isinstance(config['test_func'], tuple)
+    assert isinstance(config['test_cosmology'][1], dict)
+    assert isinstance(config['tables']['test_table_1']['test_column_3'][1], list)
