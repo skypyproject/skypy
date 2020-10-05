4c0366caafd24deddd5bbeab4028b93e50042a6f
Richard R <58728519+rrjbca@users.noreply.github.com>
2020-10-05 08:53:42 +0100

    Config parser (#304)

--- skypy/pipeline/__init__.py
+++ skypy/pipeline/__init__.py
@@ -3,4 +3,5 @@ This module provides methods to pipeline together multiple models with
 dependencies and handle their outputs.
 """
 
+from ._config import *  # noqa
 from ._pipeline import *  # noqa
