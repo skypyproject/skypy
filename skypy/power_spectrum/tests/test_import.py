30548c477c99d9b4888d2fb99872eab6b03a6f74
Nicolas Tessore <nicolas.tessore@manchester.ac.uk>
2020-10-07 16:09:29 +0100

    fix and test package imports (#334)

---
+++ skypy/power_spectrum/tests/test_import.py
@@ -0,0 +1,2 @@
+def test_import():
+    import skypy.power_spectrum
--- skypy/utils/__init__.py
+++ skypy/utils/__init__.py
@@ -4,6 +4,9 @@ This module contains utility functions.
 
 __all__ = []
 
+from . import random
+from . import special
+
 from ._decorators import (
         broadcast_arguments, dependent_argument, uses_default_cosmology,
         spectral_data_input)
