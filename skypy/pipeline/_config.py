29a2fea5ce6c2f4f481750216d37757be9461bb7
Nicolas Tessore <nicolas.tessore@manchester.ac.uk>
2020-10-20 13:45:27 +0100

    simple construction of quantities in config files (#316)
    Co-authored-by: Richard R <58728519+rrjbca@users.noreply.github.com>

--- skypy/pipeline/_config.py
+++ skypy/pipeline/_config.py
@@ -1,6 +1,8 @@
 import builtins
 from importlib import import_module
 import yaml
+import re
+from astropy.units import Quantity
 
 __all__ = [
     'load_skypy_yaml',
@@ -52,9 +54,21 @@ class SkyPyLoader(yaml.SafeLoader):
 
         return (function,) if args == '' else (function, args)
 
+    def construct_quantity(self, node):
+        value = self.construct_scalar(node)
+        return Quantity(value)
+
 
 # constructor for generic functions
 SkyPyLoader.add_multi_constructor('!', SkyPyLoader.construct_function)
+
+# constructor for quantities
+SkyPyLoader.add_constructor('!quantity', SkyPyLoader.construct_quantity)
+# Implicitly resolve quantities using the regex from astropy
+SkyPyLoader.add_implicit_resolver('!quantity', re.compile(r'''
+    \s*[+-]?((\d+\.?\d*)|(\.\d+)|([nN][aA][nN])|
+    ([iI][nN][fF]([iI][nN][iI][tT][yY]){0,1}))([eE][+-]?\d+)?[.+-]? \w* \W+
+''', re.VERBOSE), list('-+0123456789.'))
 
 
 def load_skypy_yaml(filename):
