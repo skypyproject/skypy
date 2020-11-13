030c3dff9154c94e01d27fe46ee2f298c999158f
jon <jon@thesmalluniverse.net>
2020-11-13 21:23:22 +0000

    update config tests

--- skypy/pipeline/tests/test_config.py
+++ skypy/pipeline/tests/test_config.py
@@ -44,7 +44,21 @@ def test_yaml_quantities():
 
 
 def test_keys_must_be_strings():
-    # config with keys that don't parse as Strings.
-    filename = get_pkg_data_filename('data/numeric_keys.yml')
-    with pytest.raises(ImportError):
+    # config with keys that doesn't parse as String.
+    filename = get_pkg_data_filename('data/numeric_key.yml')
+    with pytest.raises(ValueError):
+        config = load_skypy_yaml(filename)
+
+
+def test_nested_keys_must_be_strings():
+    # config with keys that doesn't parse as String.
+    filename = get_pkg_data_filename('data/numeric_nested_key.yml')
+    with pytest.raises(ValueError):
+        config = load_skypy_yaml(filename)
+
+
+def test_kwarg_must_be_strings():
+    # config with kwarg that doesn't parse as String.
+    filename = get_pkg_data_filename('data/numeric_kwarg.yml')
+    with pytest.raises(ValueError):
         config = load_skypy_yaml(filename)
