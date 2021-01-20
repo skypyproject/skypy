5a5aacc7554c11c6c2d063fb0bd0edeb8867dc30
Sarah Bridle <github@sarahbridle.net>
2021-01-20 13:59:13 +0000

    Update plot_schechter.py

--- examples/galaxies/plot_schechter.py
+++ examples/galaxies/plot_schechter.py
@@ -18,7 +18,7 @@ luminosity function as implemented in SkyPy.
 # .. math::
 #
 #    \phi(L) = \frac{{\rm d}N}{{\rm d}L{\rm d}V} =
-#    \phi_*\left(\frac{L}{L_*}\right)^\alpha{\rm exp}\left[-\frac{L}{L_*}\right]
+#    \phi_*\left(\frac{L}{L_*}\right)^\alpha{\rm exp}\left[-\frac{L}{L_*}\right].
 #
 # In SkyPy we reparameterise this distribution in terms of the absolute
 # magnitude :math:`M` of each galaxy:
@@ -27,7 +27,7 @@ luminosity function as implemented in SkyPy.
 #
 #    \phi(M) = \frac{{\rm d}N}{{\rm d}M{\rm d}V} =
 #    \frac{2}{5}{\rm log}(10)\phi_*10^{\frac{2}{5}(M_*-M)(\alpha+1)}
-#    {\rm exp}\left[-10^{\frac{2}{5}(M_*-M)}\right]
+#    {\rm exp}\left[-10^{\frac{2}{5}(M_*-M)}\right].
 #
 # In general, :math:`\phi_*`, :math:`M_*` and :math:`\alpha` can be redshift
 # dependent, resulting in a joint redshift-magnitude distribution
@@ -66,6 +66,8 @@ redshift, magnitude = schechter_lf(z_range, m_star, phi_star, alpha,
 # redshift slices to the observed B-band magnitude distribution of
 # star-forming galaxies in the ALHAMBRA survey and the median-redshift
 # model from López-Sanjuan et al. 2017.
+# The data files can be downloaded from the skypy github respository here
+# https://github.com/skypyproject/skypy/tree/master/examples/galaxies
 
 data = Table.read("lopez_sanjuan+17_B1.ecsv", format='ascii.ecsv')
 fig, ((a1, a2), (a3, a4)) = plt.subplots(nrows=2, ncols=2, constrained_layout=True)
