862af0b4ebec10930db6f560bd21925cf37235d8
Richard R <58728519+rrjbca@users.noreply.github.com>
2021-02-17 15:12:00 +0000

    Fix flake8 code style errors in halos module (#431)

--- skypy/halos/tests/test_import.py
+++ skypy/halos/tests/test_import.py
@@ -1,5 +1,5 @@
 def test_import():
-    import skypy.halos
-    import skypy.halos.abundance_matching
-    import skypy.halos.mass
-    import skypy.halos.quenching
+    import skypy.halos  # noqa: F401
+    import skypy.halos.abundance_matching  # noqa: F401
+    import skypy.halos.mass  # noqa: F401
+    import skypy.halos.quenching  # noqa: F401
