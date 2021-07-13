6481aa14250346407fa37c1feecacd20c370eaa3
Richard R <58728519+rrjbca@users.noreply.github.com>
2021-07-13 16:10:36 +0100

    DOC: Inverse transform sampling accuracy warning

--- skypy/galaxies/redshift.py
+++ skypy/galaxies/redshift.py
@@ -216,6 +216,12 @@ def redshifts_from_comoving_density(redshift, density, sky_area, cosmology, nois
     If the `noise` parameter is set to true, the number of galaxies has Poisson
     noise. If `noise` is false, the expected number of galaxies is used.
 
+    .. warning::
+        The inverse cumulative distribution function is approximated from the
+        number density and comoving volume calculated at the given `redshift`
+        values. The user must choose suitable `redshift` values to satisfy
+        their desired numerical accuracy.
+
     Parameters
     ----------
     redshift : array_like
--- skypy/utils/random.py
+++ skypy/utils/random.py
@@ -21,6 +21,12 @@ import numpy as np
 def schechter(alpha, x_min, x_max, resolution=100, size=None, scale=1.):
     """Sample from the Schechter function.
 
+    .. warning::
+        The inverse cumulative distribution function is approximated from the
+        Schechter function evaluated on a logarithmically-spaced grid. The user
+        must choose the `resolution` of this grid to satisfy their desired
+        numerical accuracy.
+
     Parameters
     ----------
     alpha : float or int
