296d750af85495fd2b27cfdd5c24d1575610567f
Lucia F. de la Bella <55983939+Lucia-Fonseca@users.noreply.github.com>
2021-09-06 12:24:53 +0100

    Modify text for clarity and delete link to reference

--- skypy/galaxies/morphology.py
+++ skypy/galaxies/morphology.py
@@ -254,7 +254,7 @@ def ryden04_ellipticity(mu_gamma, sigma_gamma, mu, sigma, size=None):
     Returns
     -------
     ellipticity: (size,) array_like
-        Ellipticity distribution sampled from the Ryden 2004 model [1].
+        Ellipticities sampled from the Ryden 2004 model.
 
     References
     ----------
