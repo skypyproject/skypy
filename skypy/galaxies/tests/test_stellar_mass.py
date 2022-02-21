6ce403b34d7a2ede465a8997b9bdb949cb070e05
Lucia F. de la Bella <55983939+Lucia-Fonseca@users.noreply.github.com>
2022-02-21 11:10:15 +0000

    Add test for the schechter parameters function

--- skypy/galaxies/tests/test_stellar_mass.py
+++ skypy/galaxies/tests/test_stellar_mass.py
@@ -63,5 +63,58 @@ def test_stellar_masses():
 
 
 def test_schechter_smf_parameters():
-    # placeholder for test
-    return 0
+    from skypy.galaxies.stellar_mass import schechter_smf_parameters
+    # Check scalar inputs
+    blue = (10**-2.423, 10**10.60, -1.21)
+    fsat_scalar, frho = 0.4, 0.2
+    sp_scalar = schechter_smf_parameters(blue, fsat_scalar, frho)
+
+    assert type(sp_scalar) == dict, 'active_parameters is not a dict: {}'.format(type(sp_scalar))
+    assert len(sp_scalar) == 4, 'The length of the tuple is not four: {}'.format(len(sp_scalar))
+    for p in sp_scalar:
+        for k in range(3):
+            assert type(sp_scalar[p][k]) is not np.ndarray, \
+                '{} tuple is not a scalar {}'.format(p, type(sp_scalar[p][k]))
+
+    # Check array input for the satellite fraction
+    fsat_array = np.array([0.4, 0.5])
+    sp_array = schechter_smf_parameters(blue, fsat_array, frho)
+    assert type(sp_array) is dict, 'active_parameters is not a dict: {}'.format(type(sp_array))
+    assert len(sp_array) == 4, 'The length of the tuple is not four: {}'.format(len(sp_array))
+    for p in sp_array:
+        assert type(sp_array[p][0]) == np.ndarray, \
+            '{} amplitude is not an array {}'.format(p, type(sp_array[p][0]))
+        assert len(sp_array[p][0]) == len(fsat_array), \
+            '{} amplitude does not match input length {}'.format(p, len(fsat_array))
+        for k in range(1, 3):
+            assert type(sp_array[p][k]) != np.ndarray, \
+                '{} slope or mstar not a scalar {}'.format(p, type(sp_array[p][k]))
+
+    # Check array input for the satellite fraction
+    amplitude_array = np.array([10**-2.4, 10**-2.3])
+    blue_array = (amplitude_array, 10**10.60, -1.21)
+    sp_array = schechter_smf_parameters(blue_array, fsat_scalar, frho)
+    assert type(sp_array) is dict, 'active_parameters is not a dict: {}'.format(type(sp_array))
+    assert len(sp_array) == 4, 'The length of the tuple is not four: {}'.format(len(sp_array))
+    for p in sp_array:
+        assert type(sp_array[p][0]) is np.ndarray, \
+            '{} amplitude is not an array {}'.format(p, type(sp_array[p][0]))
+        assert len(sp_array[p][0]) is len(fsat_array), \
+            '{} amplitude does not match input length {}'.format(p, len(amplitude_array))
+        for k in range(1, 3):
+            assert type(sp_array[p][k]) is not np.ndarray, \
+                '{} slope or mstar not a scalar {}'.format(p, type(sp_array[p][k]))
+
+    # Corner cases
+    # Case I: no satellite galaxies
+    sp_sat0 = schechter_smf_parameters(blue, 0, frho)
+    assert sp_sat0['centrals'][0] == sp_sat0['mass_quenched'][0] == blue[0], \
+        'The amplitude of centrals and mass-quenched are not equal to the \
+         amplitude of the blue sample'
+    assert sp_sat0['satellites'][0] == sp_sat0['satellite_quenched'][0] == 0, \
+        'The amplitude of satellites and satellite-quenched are not zero'
+
+    # Case II: no satellite-quenched galaxies
+    sp_rho0 = schechter_smf_parameters(blue, fsat_scalar, 0)
+    assert - sp_rho0['satellite_quenched'][0] == 0, \
+        'The satellite-quenched is not zero {}'.format(sp_rho0['satellite_quenched'][0])
