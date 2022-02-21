dffd2df256e799dd22ef66bda5212afce6757eb1
Lucia F. de la Bella <55983939+Lucia-Fonseca@users.noreply.github.com>
2022-02-21 15:48:56 +0000

    Create plot example for galaxy demographics

---
+++ examples/galaxies/plot_demographics.py
@@ -0,0 +1,31 @@
+"""
+Galaxy Demographics
+===================
+This example demonstrates how to sample any type of galaxy population
+from a general Schechter mass function as implemented in SkyPy.
+"""
+
+# %%
+# Schechter Parameters
+# --------------------
+
+import numpy as np
+from skypy.galaxies import *
+
+# %%
+# Weigel et al 2016 Model
+# -----------------------
+#
+# Here we compare our sampled galaxies.
+# STRAUSS clip!
+
+
+
+# %%
+# References
+# ----------
+#
+#
+# .. [1] de la Bella+2021.
+# 
+# .. [2] Weigel 2016
