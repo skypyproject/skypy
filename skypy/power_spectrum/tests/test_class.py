cec0fb92d7073a79f66fc1aeac237dde5787472f
Ian Harrison <itrharrison@gmail.com>
2022-07-01 14:36:46 +0100

    test classy class, instead of classy func

--- skypy/power_spectrum/tests/test_class.py
+++ skypy/power_spectrum/tests/test_class.py
@@ -22,24 +22,28 @@ def test_classy():
     '''
     Test a default astropy cosmology
     '''
-    from skypy.power_spectrum import classy
+    from skypy.power_spectrum import CLASSY
 
     Pl15massless = Planck15.clone(name='Planck 15 massless neutrino', m_nu=[0., 0., 0.]*u.eV)
 
     redshift = [0.0, 1.0]
     wavenumber = np.logspace(-4.0, np.log10(2.0), 200)
-    pzk = classy(wavenumber, redshift, Pl15massless)
+    k_max = wavenumber.max()
+    ps = CLASSY(k_max, redshift, Pl15massless)
+    pzk = ps(wavenumber, redshift)
     assert pzk.shape == (len(redshift), len(wavenumber))
     assert allclose(pzk, test_pzk, rtol=1.e-4)
 
     # also check redshifts are ordered correctly
     redshift = [1.0, 0.0]
-    pzk = classy(wavenumber, redshift, Pl15massless)
+    ps = CLASSY(k_max, redshift, Pl15massless)
+    pzk = ps(wavenumber, redshift)
     assert pzk.shape == (len(redshift), len(wavenumber))
     assert allclose(pzk, test_pzk[np.argsort(redshift)], rtol=1.e-4)
 
     # also check scalar arguments are treated correctly
     redshift = 1.0
     wavenumber = 1.e-1
-    pzk = classy(wavenumber, redshift, Pl15massless)
+    ps = CLASSY(k_max, redshift, Pl15massless)
+    pzk = ps(wavenumber, redshift)
     assert np.isscalar(pzk)
