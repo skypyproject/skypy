8ea593e01f8861584c6fba60edf4d0fff28a3937
Ian Harrison <itrharrison@gmail.com>
2022-08-18 17:31:35 +0100

    Update test_skypy.py
    E275 fixes

--- skypy/pipeline/tests/test_skypy.py
+++ skypy/pipeline/tests/test_skypy.py
@@ -58,7 +58,7 @@ def test_logging(capsys, tmp_path):
     output_filename = str(tmp_path / 'logging.fits')
     skypy.main([config_filename, output_filename])
     out, err = capsys.readouterr()
-    assert(not err)
+    assert (not err)
 
     # Run again with increased verbosity and capture log. Force an exception by
     # not using the "--overwrite" flag when the output file already exists.
@@ -78,19 +78,19 @@ def test_logging(capsys, tmp_path):
     # Check all jobs appear in the log
     for job in list(config) + list(tables) + columns:
         log_string = f"[INFO] skypy.pipeline: Generating {job}"
-        assert(log_string in err)
+        assert (log_string in err)
 
     # Check all functions appear in the log
     for f in functions:
         log_string = f"[INFO] skypy.pipeline: Calling {f.function.__name__}"
-        assert(log_string in err)
+        assert (log_string in err)
 
     # Check cosmology appears in the log
     if cosmology:
-        assert("[INFO] skypy.pipeline: Setting cosmology" in err)
+        assert ("[INFO] skypy.pipeline: Setting cosmology" in err)
 
     # Check writing output file is in the log
-    assert(f"[INFO] skypy: Writing {output_filename}" in err)
+    assert (f"[INFO] skypy: Writing {output_filename}" in err)
 
     # Check error for existing output file is in the log
     try:
@@ -100,10 +100,10 @@ def test_logging(capsys, tmp_path):
     except ImportError:
         # Fallback on old error message from astropy v4.x
         error_string = f"[ERROR] skypy: File {output_filename!r} already exists."
-    assert(error_string in err)
+    assert (error_string in err)
 
     # Run again with decreased verbosity and check the log is empty
     with pytest.raises(SystemExit):
         skypy.main([config_filename, output_filename, '-qq'])
     out, err = capsys.readouterr()
-    assert(not err)
+    assert (not err)
