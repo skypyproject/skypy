0cd2493db0988f454d792b9f27f7a0b6720474dc
vikram <bhamrev@stpaulsschool.org.uk>
2022-09-04 21:12:02 +0100

    initial commit for pr

---
+++ skypy/galaxies/tests/test_vdf_sampler.py
@@ -0,0 +1,6 @@
+from lens_population_sampler import vdf_sampler
+import numpy as np
+
+def test_vdf_sampler():
+    samples = vdf_sampler.sample_vdf(100, 200, size = 1000)
+    assert np.sum((100 <= samples) & (samples <= 200)) == 1000
